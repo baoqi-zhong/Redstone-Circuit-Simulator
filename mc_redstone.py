29b7429744bc8309f248099aad44b335b5c0c8cb
baoqi-zhong <1685105352@qq.com>
2021-02-18 22:39:47 +0800

    add redstone torch logic and fix bugs

--- mc_redstone.py
+++ mc_redstone.py
@@ -10,6 +10,8 @@ REDSTONE = 2
 TORCH = 3
 REPEATER = 4
 # 不同的方向用world_data标识
+
+
 class Window(pyglet.window.Window):
     def __init__(self, *args, **kwargs):
         super(Window, self).__init__(*args, **kwargs)
@@ -18,6 +20,8 @@ class Window(pyglet.window.Window):
         self.layer = 0
         self.mouse_press = 0  #0没有 1左键 2右键
         self.now_block = BLOCK
+
+        self.last_focus = [0,0]
 
 
         pyglet.clock.schedule_interval(self.update, 1.0 / 60)  # 每秒刷新60次
@@ -49,17 +53,20 @@ class Window(pyglet.window.Window):
         if 88<x<928 and 0<y<560:
             x_ = min((x - 89) // 28, 29)
             y_ = min(y // 28, 19)
-            if self.mouse_press == 1:
-                self.add_block(x_, y_, self.now_block)
-            elif self.mouse_press == 2:
-                self.delete_block(x_, y_)
+            if [x_, y_] != self.last_focus:
+                if self.mouse_press == 1:
+                    self.add_block(x_, y_, self.now_block)
+                elif self.mouse_press == 2:
+                    self.delete_block(x_, y_)
+
+                self.last_focus = [x_, y_]
 
     def on_key_press(self, symbol, modifiers):
         if 0x030 < symbol < 0x035:
             self.now_block = symbol-0x030  # key.py里面0对应值为0x030
 
-
     # ------------------------------------------------------
+
     def test_block(self, x, y, layer, block_ids):
         # 检测某位置的方块 防止数组越界
         if 0<=x<30 and 0<=y<20 and self.world_block[layer][y][x] in block_ids:
@@ -76,7 +83,7 @@ class Window(pyglet.window.Window):
                 block_data[1] = self.test_block(x + 1, y, self.layer, [2, 3])
                 block_data[2] = self.test_block(x, y - 1, self.layer, [2, 3])
                 block_data[3] = self.test_block(x - 1, y, self.layer, [2, 3])
-                if sum(block_data) == 1:
+                if sum(block_data[:4]) == 1:
                     if block_data[0] or block_data[2]:
                         block_data[0] = block_data[2] = 1
                     if block_data[1] or block_data[3]:
@@ -86,10 +93,15 @@ class Window(pyglet.window.Window):
                 self.world_data[self.layer][y][x] = block_data
 
         update_redstone_shape(x, y)
-        update_redstone_shape(x - 1, y)
-        update_redstone_shape(x + 1, y)
-        update_redstone_shape(x, y - 1)
-        update_redstone_shape(x, y + 1)
+        # 防止越界
+        if x - 1 > 0:
+            update_redstone_shape(x - 1, y)
+        if x + 1 < 30:
+            update_redstone_shape(x + 1, y)
+        if y - 1 > 0:
+            update_redstone_shape(x, y - 1)
+        if y + 1 < 20:
+            update_redstone_shape(x, y + 1)
 
     def update_redstone_energy(self):
         flag = False
@@ -103,37 +115,86 @@ class Window(pyglet.window.Window):
                     surrounding.append(self.test_block(x, y - 1, self.layer, [2, 3]) * self.world_data[self.layer][y - 1][x][4])
                     surrounding.append(self.test_block(x - 1, y, self.layer, [2, 3]) * self.world_data[self.layer][y][x - 1][4])
 
-                    if self.world_data[self.layer][y][x][4] != max(max(surrounding)-1,0):
-                        self.world_data[self.layer][y][x][4] = max(max(surrounding)-1,0)
+                    if self.world_data[self.layer][y][x][4] != max(max(surrounding)-1, 0):
+                        self.world_data[self.layer][y][x][4] = max(max(surrounding)-1, 0)
                         flag = True
         if flag:
             self.update_redstone_energy()
 
+    def update_torch_around(self, x, y):
+        if self.test_block(x, y + 1, self.layer, [3]) and 1 in self.world_data[self.layer][y + 1][x][:4] and \
+                self.world_data[self.layer][y + 1][x][:4].index(1) == 2:
+            self.delete_block(x, y + 1)
+        if self.test_block(x + 1, y, self.layer, [3]) and 1 in self.world_data[self.layer][y][x + 1][:4] and \
+                self.world_data[self.layer][y][x + 1][:4].index(1) == 3:
+            self.delete_block(x + 1, y)
+        if self.test_block(x, y - 1, self.layer, [3]) and 1 in self.world_data[self.layer][y - 1][x][:4] and \
+                self.world_data[self.layer][y - 1][x][:4].index(1) == 0:
+            self.delete_block(x, y - 1)
+        if self.test_block(x - 1, y, self.layer, [3]) and 1 in self.world_data[self.layer][y][x - 1][:4] and \
+                self.world_data[self.layer][y][x - 1][:4].index(1) == 1:
+            self.delete_block(x - 1, y)
 
     def add_block(self, x, y, block_id):
-        self.world_block[self.layer][y][x] = block_id
-        self.world_data[self.layer][y][x] = [0, 0, 0, 0, 0]
-        if block_id ==3:
+        def find_torch_ok_list(x, y):
+            ok_list = []
+            if self.test_block(x, y + 1, self.layer, [1]):
+                ok_list.append(0)
+            if self.test_block(x + 1, y, self.layer, [1]):
+                ok_list.append(1)
+            if self.test_block(x, y - 1, self.layer, [1]):
+                ok_list.append(2)
+            if self.test_block(x - 1, y, self.layer, [1]):
+                ok_list.append(3)
+            return ok_list
+
+        update_torch_flag = False
+        if block_id != 1:
+            update_torch_flag = True
+
+        if block_id == 3:  # torch
+            if self.world_block[self.layer][y][x] == block_id:
+                ok_list = find_torch_ok_list(x, y)
+                if ok_list:
+                    if sum(self.world_data[self.layer][y][x][:4]) == 0:
+                        self.world_data[self.layer][y][x][ok_list[0]] = 1
+
+                    else:
+                        print(self.world_data[self.layer][y][x])
+                        this = ok_list.index(self.world_data[self.layer][y][x].index(1))  # 当前状态对应ok_list中的第几个
+                        self.world_data[self.layer][y][x][ok_list[this]] = 0
+                        if this+1 < len(ok_list):
+                            self.world_data[self.layer][y][x][ok_list[this+1]] = 1
+            else:
+                self.world_data[self.layer][y][x][:4] = [0, 0, 0, 0]
+
             self.world_data[self.layer][y][x][4] = 16
 
+        self.world_block[self.layer][y][x] = block_id
+
         if block_id == 2 or block_id == 3:  # 红石
-            self.update_redstone_shape_around(x, y)
+            self.update_redstone_shape_around(x, y)  # 只会动rs
 
-
+        if update_torch_flag:
+            self.update_torch_around(x, y)
 
     def delete_block(self, x, y):
-        block_id = self.world_block[self.layer][y][x]+0
+        update_redstone_shape_flag = False
+        if self.world_block[self.layer][y][x] in [2, 3, 4]:
+            update_redstone_shape_flag = True
+        if self.world_block[self.layer][y][x] == 1:
+            self.update_torch_around(x, y)
 
         self.world_block[self.layer][y][x] = EMPTY
-        self.world_data[self.layer][y][x][4] = 0
-        if block_id == 2 or block_id == 3:  # 红石
+        self.world_data[self.layer][y][x] = [0, 0, 0, 0, 0]
+        if update_redstone_shape_flag:
             self.update_redstone_shape_around(x, y)
+
+
 
     def update(self, dt):
         # update是在init里面定义的 不是重写的
-        stime = time.time()
         self.update_redstone_energy()
-        print(time.time()-stime)
 
     def draw_bg(self):
         glColor3f(0.93, 0.93, 0.93)
@@ -155,7 +216,7 @@ class Window(pyglet.window.Window):
         glEnd()
 
 
-    def draw_block(self,x , y, block_id):
+    def draw_block(self, x , y, block_id):
         # xy指的是编号，不是坐标。
         # 一个block占28x28像素 包括边框各2像素
         # 中心方块24x24
@@ -223,6 +284,7 @@ class Window(pyglet.window.Window):
             # data 是数字
             x = x*28+91
             y = y*28+1
+
             def draw_point():
                 glBegin(GL_POLYGON)
                 glColor3f(1, 0, 0)
@@ -264,16 +326,13 @@ class Window(pyglet.window.Window):
         self.text.text = '1\n方块\n\n2\n红石\n\n3\n火把\n\n4\n中继器\n\n\n现在\n'+['方块', '红石', '火把', '中继器'][self.now_block-1]
         self.text.draw()
 
-
     def debug(self):
         [pyglet.text.Label(str(self.world_data[self.layer][y][x][4]), font_size=15, x=x * 28 + 91, y=y* 28 + 1, width=28, color=(0,0,0,255)).draw() for x in range(30) for y in range(20)]
 
 
-
-
-
-window = Window(width=1000, height=600, caption='Pyglet', resizable=False)
-pyglet.app.run()
+if __name__ == '__main__':
+    window = Window(width=1000, height=600, caption='Pyglet', resizable=False)
+    pyglet.app.run()
 # 30x20
 # window坐标起点在左下角!!!!!!!!!!
 
